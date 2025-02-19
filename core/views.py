b55efe71cb1f0c169d92b8135db8beb4c5124d00
Rokhaya Mbaye <Rokhaya.mbaye@esums.tech>
2025-02-19 18:19:22 +0000

    initial commit

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+def hellotime(request):
+    now = datetime.datetime.now()
+    return HttpResponse(f" <h1>Hello, world!</h1> <p>It's {now}.</p>")
+
+def screenprint(request):
+    return render(request, "core/screenprint.html")
+
+
+# TODO: add anoher view for the blackbox page
